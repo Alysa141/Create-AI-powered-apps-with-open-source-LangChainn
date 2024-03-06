038654d6af99b56c1dad33c7778387d5c79f9669
Alysa141 <160560953+Alysa141@users.noreply.github.com>
2024-03-06 14:17:52 +0000

    project finished

--- chain_customerSupport.py
+++ chain_customerSupport.py
@@ -5,7 +5,7 @@ from langchain.prompts import PromptTemplate
 import os
 
 # Mengatur LLM
-os.environ["OPENAI_API_KEY"] = "API KEY KAMU"
+os.environ["OPENAI_API_KEY"] = "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"
 llm = ChatOpenAI(temperature=0.9)
 
 def handle_complaint(komplain: str) -> str:
